61dec1707888834f4ad4aab486045f1443b53fb8
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-04-12 20:48:29 -0400

    add Axis and camera, and fixed bug abrir Menu

--- Src/Models/3D/Composite/Escenario.cs
+++ Src/Models/3D/Composite/Escenario.cs
@@ -2,15 +2,31 @@ using System;
 using OpenTK.Mathematics;
 using OpenTKGUI.Src.Models._2D;
 using OpenTKGUI.Src.Models._3D;
+using OpenTKGUI.Src.Utils;
 
 namespace OpenTKGUI.Src.Models._3D.Composite;
 
 public class Escenario
 {
     public List<Objeto> Objetos { get; } = [];
+    public Axis Ejes;
+    public Escenario() { }
+    public Escenario(ArcRotateCamera camera)
+    {
+        Ejes = new Axis(camera);
+        Ejes.Load();
+    }
+    public bool EsNombreRepetido(string name)
+    {
+        return Objetos.Any(o => o.Name == name);
+    }
 
     public void Add(Objeto objeto)
     {
+        if (EsNombreRepetido(objeto.Name))
+        {
+            throw new Exception($"Ya existe un objeto con el nombre {objeto.Name}");
+        }
         Objetos.Add(objeto);
     }
 
@@ -20,6 +36,11 @@ public class Escenario
         {
             objeto.Draw();
         }
+    }
+    public void DrawEjes()
+    {
+        Ejes.Bind();
+        Ejes.Draw();
     }
 
     public Objeto GetObjeto(string name)
